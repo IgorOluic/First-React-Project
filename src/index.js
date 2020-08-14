dafe5aa6c9b54de00180004bf146aa2912679c5f
IgorOluic <igoroluic13@gmail.com>
2020-08-14 17:23:26 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import InputField from "./components/inputField";
+import DisplayContent from "./components/displayContent";
+import React, { useState } from "react";
+import "./custom.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [input, setInput] = useState("");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div className="input-container">
+      <InputField onClick={(value) => setInput(value)} />
+      <DisplayContent value={input} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
