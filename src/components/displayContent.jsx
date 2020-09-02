ae023652add7001dfdceccb3a961796cb9a4a9e4
IgorOluic <igoroluic13@gmail.com>
2020-09-02 17:45:22 +0200

    made changes for redux implementation

--- src/components/displayContent.jsx
+++ src/components/displayContent.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import "../custom.css";
+import { connect } from "react-redux";
 
 const DisplayContent = (props) => {
-  if (!props.value) {
-    return <div className="content-container"></div>;
-  } else {
+  if (props.name) {
     return (
       <div className="content-container">
-        <p className="content-paragraph">{props.value}</p>
+        <p className="content-name">{props.name}</p>
+        <p className="content-gender">Gender: {props.gender}</p>
+        <p className="content-age">Average age: {props.age}</p>
+        <p className="content-countries">Countries: {props.countries}</p>
       </div>
     );
+  } else {
+    return null;
   }
 };
 
-export default DisplayContent;
+const mapStateToProps = (state) => {
+  return {
+    name: state.name,
+    gender: state.gender,
+    age: state.age,
+    countries: state.countries,
+  };
+};
+
+export default connect(mapStateToProps)(DisplayContent);
