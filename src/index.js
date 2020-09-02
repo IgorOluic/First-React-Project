ecf31522fa1a1672c7b10016ac739f0c79145e1e
IgorOluic <igoroluic13@gmail.com>
2020-09-02 17:48:13 +0200

    made changes for redux implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/reducers";
 import InputField from "./components/inputField";
 import DisplayContent from "./components/displayContent";
-import React, { useState } from "react";
+import React from "react";
 import "./custom.css";
 
-const App = () => {
-  const [input, setInput] = useState("");
+let store = createStore(reducers, applyMiddleware(thunk));
 
+const App = () => {
   return (
     <div className="input-container">
-      <InputField onClick={(value) => setInput(value)} />
-      <DisplayContent value={input} />
+      <InputField />
+      <DisplayContent />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
