9b136f2d8fc4c535d28233d50744301bbb6f7384
IgorOluic <igoroluic13@gmail.com>
2020-09-11 19:36:48 +0200

    added new state properties

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,18 +1,24 @@
+import { UPDATE_INFO } from "../action-types/index";
+
 let defaultState = {
   name: "",
   countries: "",
   age: "",
   gender: "",
+  countryIds: [],
+  countryPopularity: [],
 };
 
 const mainReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "NEW_DATA": {
+    case UPDATE_INFO: {
       return {
         name: action.name,
         countries: action.countries,
         age: action.age,
         gender: action.gender,
+        countryIds: action.countryIds,
+        countryPopularity: action.countryPopularity,
       };
     }
     default:
