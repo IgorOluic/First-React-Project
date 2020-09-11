9b882119e99198fe835b2793f5f7a13bb831c52e
IgorOluic <igoroluic13@gmail.com>
2020-09-11 19:32:49 +0200

    separated actions from other logic

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,44 +1,21 @@
-import axios from "axios";
-import dataValidator from "./dataValidator";
+import requestApi from "../api/request";
+import { UPDATE_INFO } from "../action-types/index";
 
-export function loadName() {
-  let input = document.getElementById("input-id").value;
-
-  if (input) {
-    let nationalize = "https://api.nationalize.io?name=" + input;
-    let agify = "https://api.agify.io?name=" + input;
-    let genderize = "https://api.genderize.io?name=" + input;
-
-    let requestNationalize = axios.get(nationalize);
-    let requestAgify = axios.get(agify);
-    let requestGenderize = axios.get(genderize);
-
-    return (dispatch) => {
-      return axios
-        .all([requestNationalize, requestAgify, requestGenderize])
-        .then(
-          axios.spread((...responses) => {
-            const nationalizeData = responses[0];
-            const agifyData = responses[1];
-            const genderizeData = responses[2];
-
-            dispatch(
-              dataValidator(
-                nationalizeData.data.name,
-                nationalizeData.data.country,
-                agifyData.data.age,
-                genderizeData.data.gender
-              )
-            );
-          })
-        )
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-  } else {
-    return (dispatch) => {
-      dispatch(() => null);
-    };
-  }
+export function loadNameInfo() {
+  return (dispatch) => {
+    try {
+      requestApi().then((data) => {
+        dispatch(updateInfo(data));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
+
+const updateInfo = (data) => {
+  return {
+    type: UPDATE_INFO,
+    ...data,
+  };
+};
