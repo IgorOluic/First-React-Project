1c324e4c02041b04743a1fd31560b0addc483a26
IgorOluic <igoroluic13@gmail.com>
2020-09-20 17:58:44 +0200

    created new actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,9 @@
 import requestApi from "../api/request";
-import { UPDATE_INFO } from "../action-types/index";
+import {
+  UPDATE_INFO,
+  UPDATE_MAP_COORDS,
+  UPDATE_GLOBE_COORDS,
+} from "../action-types/index";
 
 export function loadNameInfo() {
   return (dispatch) => {
@@ -17,5 +21,19 @@ const updateInfo = (data) => {
   return {
     type: UPDATE_INFO,
     ...data,
+  };
+};
+
+export const updateGlobeCoords = (coords) => {
+  return {
+    type: UPDATE_GLOBE_COORDS,
+    globeCoords: coords,
+  };
+};
+
+export const updateMapCoords = (coords) => {
+  return {
+    type: UPDATE_MAP_COORDS,
+    mapCoords: coords,
   };
 };
