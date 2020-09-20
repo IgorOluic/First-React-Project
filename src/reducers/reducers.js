5493406f56dc174d12007f06264649dda47ec11e
IgorOluic <igoroluic13@gmail.com>
2020-09-20 18:39:49 +0200

    added new state properties and dispatch handlers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,24 +1,43 @@
-import { UPDATE_INFO } from "../action-types/index";
+import {
+  UPDATE_GLOBE_COORDS,
+  UPDATE_INFO,
+  UPDATE_MAP_COORDS,
+} from "../action-types/index";
 
 let defaultState = {
   name: "",
-  countries: "",
   age: "",
   gender: "",
+  countries: [],
   countryIds: [],
-  countryPopularity: [],
+  namePopularity: [],
+  globeCoords: [0, 0],
+  mapCoords: [0, 0],
 };
 
 const mainReducer = (state = defaultState, action) => {
   switch (action.type) {
     case UPDATE_INFO: {
       return {
+        ...state,
         name: action.name,
         countries: action.countries,
         age: action.age,
         gender: action.gender,
         countryIds: action.countryIds,
-        countryPopularity: action.countryPopularity,
+        namePopularity: action.namePopularity,
+      };
+    }
+    case UPDATE_GLOBE_COORDS: {
+      return {
+        ...state,
+        globeCoords: action.globeCoords,
+      };
+    }
+    case UPDATE_MAP_COORDS: {
+      return {
+        ...state,
+        mapCoords: action.mapCoords,
       };
     }
     default:
